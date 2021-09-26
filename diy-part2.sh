0c3fa9ce8424118bc9366c3080dfd345f89f0a67
shmily103 <supanshui@gmail.com>
2021-09-26 20:25:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,7 +16,7 @@ sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac802
 sed -i 's/ssid=OpenWrt/ssid=Panzy/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 #add packages
-git clone https://github.com/shmily103/Panzy.git package/Panzy
+git clone https://github.com/shmily103/Panzy-backup.git package/Panzy
 # Modify Automount
 \cp -rf package/Panzy/mount.hotplug package/system/fstools/files
 # fix unblockmusic
